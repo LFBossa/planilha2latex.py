60decc470f86f6af49103fe30664710134a6b629
Bossa <luiznandob@hotmail.com>
2021-06-29 16:10:36 -0300

    Adicionada a chave "D"

--- ferramentas/filtros.py
+++ ferramentas/filtros.py
@@ -15,6 +15,7 @@ MAPA_DATAHORA = {
     "dia": "%A"
 }
 
+_D = lambda x: str(x.day)
 _Ds =  lambda x: x.strftime("%a").capitalize()
 _Dia = lambda x: x.strftime("%A").capitalize()
 _Mes = lambda x: x.strftime("%B").capitalize()
@@ -26,7 +27,8 @@ MAPA_DATAHORA_EXTRA = {
     "Ds": _Ds,
     "Dia-feira": _Diafeira, 
     "Dia": _Dia,
-    "dia-feira": _diafeira
+    "dia-feira": _diafeira,
+    "D": _D,
 }
 
 
@@ -38,9 +40,9 @@ def map_to_strf(texto):
 
 def formato(datetime, fmt="%x"): 
     strfmt = map_to_strf(fmt)
-    for key, val in MAPA_DATAHORA_EXTRA.items():
+    for key, func in MAPA_DATAHORA_EXTRA.items():
         if key in strfmt:
-            strfmt = strfmt.replace(key, val(datetime))
+            strfmt = strfmt.replace(key, func(datetime))
     return datetime.strftime(strfmt)
 
 def teste1():
@@ -49,7 +51,7 @@ def teste1():
     assert saida1 == "%a, %d de %b de %Y"
 
 if __name__ == '__main__':
-    texto = "Dia-feira, DD de Mês de AAAA (HHhmm)"
+    texto = "Dia, DD de mês de AAAA (HHhmm)"
     fmt = map_to_strf(texto)
     print(fmt)
     print(formato(datetime.now(),fmt))
